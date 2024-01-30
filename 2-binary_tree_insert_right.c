2b61b9781b6dede0e504881e8bdb3a9532fc776e
IA-LINK <jyc.kom@gmail.com>
2024-01-30 16:07:42 -0500

    task one to five

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - funcio to insert node to th right of a node
+ * @parent: this point to the parent of the node
+ * @value: this point to the value of the node
+ * 
+ * Return: this return created node or NULL on failure
+*/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *right_node;
+
+    if (parent == NULL)
+    {
+        return (NULL); /* Insertion failure */
+    }
+    
+    right_node = malloc(sizeof(binary_tree_t));
+
+    if (right_node == NULL)
+    {
+        return (NULL); /* Allocation failure */
+    }
+    
+    right_node->parent = parent;
+    right_node->n = value;
+    right_node->left = NULL;
+    right_node->right = parent->right;
+
+    if (parent->right != NULL)
+    {
+        right_node->right = parent->right;
+        parent->right->parent = right_node;
+    }
+
+    parent->right = right_node;
+    return (right_node);
+}
---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - this function delete an entire binary tree
+ * @tree: thi is the tree to be deleted
+ * 
+ * Return: Nothing
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return (NULL); /* do nothing*/
+    }
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+
+    free(tree);
+}
